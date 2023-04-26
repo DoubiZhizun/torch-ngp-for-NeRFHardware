07b8eef96c280c7113222b48aa7014f262d72322
zhaobz <1411203632@qq.com>
2023-04-26 14:04:14 +0800

    file format

--- test_all.bat
+++
@@ -1,8 +0,0 @@
-python main_nerf.py ../nerf_synthetic/chair --workspace nerf_workspace/chair --cuda_ray --preload --bound 1 --scale 0.66 --dt_gamma 0 --iters 160000 --max_steps 1024 --test
-python main_nerf.py ../nerf_synthetic/drums --workspace nerf_workspace/drums --cuda_ray --preload --bound 1 --scale 0.66 --dt_gamma 0 --iters 160000 --max_steps 1024 --test
-python main_nerf.py ../nerf_synthetic/ficus --workspace nerf_workspace/ficus --cuda_ray --preload --bound 1 --scale 0.66 --dt_gamma 0 --iters 160000 --max_steps 1024 --test
-python main_nerf.py ../nerf_synthetic/hotdog --workspace nerf_workspace/hotdog --cuda_ray --preload --bound 1 --scale 0.66 --dt_gamma 0 --iters 160000 --max_steps 1024 --test
-python main_nerf.py ../nerf_synthetic/lego --workspace nerf_workspace/lego --cuda_ray --preload --bound 1 --scale 0.66 --dt_gamma 0 --iters 160000 --max_steps 1024 --test
-python main_nerf.py ../nerf_synthetic/materials --workspace nerf_workspace/materials --cuda_ray --preload --bound 1 --scale 0.66 --dt_gamma 0 --iters 160000 --max_steps 1024 --test --test
-python main_nerf.py ../nerf_synthetic/mic --workspace nerf_workspace/mic --cuda_ray --preload --bound 1 --scale 0.66 --dt_gamma 0 --iters 160000 --max_steps 1024 --test
-python main_nerf.py ../nerf_synthetic/ship --workspace nerf_workspace/ship --cuda_ray --preload --bound 1 --scale 0.66 --dt_gamma 0 --iters 160000 --max_steps 1024 --test
